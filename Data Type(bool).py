2cd0e5952b23089e465389a7e81a242a3e64985d
FB1937 <choidaehoon0406@gmail.com>
2023-09-14 22:22:15 +0900

    all

---
+++ Data Type(bool).py
@@ -0,0 +1,16 @@
+# 파이썬 프로그래밍의 기초 자료형
+
+# 불 자료형
+
+# (1) 참과 거짓을 나타냄
+print("(1)--------------------")
+a = True
+b = False
+print(type(a), type(b))
+print(1==1, 1==2)
+
+# (2) 자료형의 참, 거짓
+print("(2)--------------------")
+print(bool("python"), bool(""))
+print(bool([1,2,3]), bool([]))
+print(bool(3), bool(0))
